60b88a7dc75d7b64658456270861fb04422a3520
sinyoung0403 <tlstjs753@gmail.com>
2025-05-19 18:06:39 +0900

    feat/#4 : Repository 구현

---
+++ src/main/java/com/inity/tickenity/domain/reservation/repository/ReservationRepository.java
@@ -0,0 +1,8 @@
+package com.inity.tickenity.domain.reservation.repository;
+
+import com.inity.tickenity.domain.common.repository.BaseRepository;
+import com.inity.tickenity.domain.reservation.entity.Reservation;
+
+public interface ReservationRepository extends BaseRepository<Reservation, Long> {
+
+}
