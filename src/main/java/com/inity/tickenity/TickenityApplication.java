7744c9d7595898356bec971e9ee82a0982db6784
SeungWoo Ryu <swstar21c@gmail.com>
2025-05-19 12:20:04 +0900

    feat/#1 : init commit

---
+++ src/main/java/com/inity/tickenity/TickenityApplication.java
@@ -0,0 +1,13 @@
+package com.inity.tickenity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickenityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickenityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inity/tickenity/TickenityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inity.tickenity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickenityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
