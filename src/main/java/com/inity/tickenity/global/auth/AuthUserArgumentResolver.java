4f55da34174de61932c2d72143894910998ec41c
SeungWoo Ryu <swstar21c@gmail.com>
2025-05-19 21:46:19 +0900

    feat/#8 : @Auth 기반 AuthUserArgumentResolver 구현 및 등록

---
+++ src/main/java/com/inity/tickenity/global/auth/AuthUserArgumentResolver.java
@@ -0,0 +1,48 @@
+package com.inity.tickenity.global.auth;
+
+import com.inity.tickenity.domain.common.annotation.Auth;
+import com.inity.tickenity.domain.common.dto.AuthUser;
+import com.inity.tickenity.domain.user.enums.UserRole;
+import com.inity.tickenity.global.exception.BusinessException;
+import com.inity.tickenity.global.response.ResultCode;
+import io.micrometer.common.lang.Nullable;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+public class AuthUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean hasAuthAnnotation = parameter.getParameterAnnotation(Auth.class) != null;
+        boolean isAuthUserType = parameter.getParameterType().equals(AuthUser.class);
+
+        // @Auth 어노테이션과 AuthUser 타입이 함께 사용되지 않은 경우 예외 발생
+        if (hasAuthAnnotation != isAuthUserType) {
+            throw new BusinessException(ResultCode.AUTH_ANNOTATION_USER_MISMATCH);
+        }
+
+        return hasAuthAnnotation;
+    }
+
+    @Override
+    public Object resolveArgument(
+            @Nullable MethodParameter parameter,
+            @Nullable ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest,
+            @Nullable WebDataBinderFactory binderFactory
+    ) {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+
+        // JwtFilter 에서 set 한 userId, email, userRole 값을 가져옴
+        Long userId = (Long) request.getAttribute("userId");
+        String email = (String) request.getAttribute("email");
+        UserRole userRole = UserRole.of((String) request.getAttribute("userRole"));
+
+        return new AuthUser(userId, email, userRole);
+    }
+
+}
