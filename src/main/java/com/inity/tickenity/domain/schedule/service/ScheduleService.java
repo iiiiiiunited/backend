7f61f567fae6080d8f0d3842956e4f5214db9959
yeeun0702 <kimyeeun0702@naver.com>
2025-05-19 21:38:31 +0900

    feat/#6 : 콘서트 별 일정조회 비즈니스 로직 구현

---
+++ src/main/java/com/inity/tickenity/domain/schedule/service/ScheduleService.java
@@ -0,0 +1,26 @@
+package com.inity.tickenity.domain.schedule.service;
+
+import com.inity.tickenity.domain.schedule.dto.response.ScheduleResponseDto;
+import com.inity.tickenity.domain.schedule.entity.Schedule;
+import com.inity.tickenity.domain.schedule.repository.ScheduleRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ScheduleService {
+    private final ScheduleRepository scheduleRepository;
+
+    public List<ScheduleResponseDto> getSchedulesByConcert(Long concertId) {
+        List<Schedule> schedules = scheduleRepository.findByConcertId(concertId);
+        return schedules.stream()
+                .map(s -> new ScheduleResponseDto(s.getId(), s.getStartTime(), s.getEndTime()))
+                .toList();
+    }
+
+
+}
