37aa647bf0f279c76adc5631e0fc445da8262ba6
YongLeeCode <yonghwan311@gmail.com>
2025-05-20 12:11:57 +0900

    refactor#5 : 사용되지 않는 코드 제거

--- src/main/java/com/inity/tickenity/domain/concertvenue/ConcertVenueRepository.java
+++ src/main/java/com/inity/tickenity/domain/concertvenue/ConcertVenueRepository.java
@@ -1,11 +1,6 @@
 package com.inity.tickenity.domain.concertvenue;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.inity.tickenity.domain.concert.entity.Concert;
-
 public interface ConcertVenueRepository extends JpaRepository<ConcertVenue, Long> {
-	List<ConcertVenue> findByConcert(Concert concert);
 }
